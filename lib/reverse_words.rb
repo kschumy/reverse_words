b27241505b2f4520b4f67d76e36f6fb6090084fd
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-27 21:20:33 -0700

    revised solutions

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,25 +1,25 @@
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
   return my_words if my_words == nil || my_words.length < 2
-  index = 0
-  word_start = 0
-  while index <= my_words.length
-    if my_words[index] == " " || my_words[index] == nil
-      my_words = reverse_helper(word_start, index - 1, my_words) if word_start > 0
-      word_start = 0
+  curr_index = 0
+  letter_count = 0
+  while curr_index <= my_words.length
+    if my_words[curr_index] == " " || my_words[curr_index] == nil
+      reverse_helper(curr_index, curr_index - letter_count, my_words) if letter_count > 0
+      letter_count = 0
     else
-      word_start += 1
+      letter_count += 1
     end
-    index += 1
+    curr_index += 1
   end
   return my_words
 end
 
-def reverse_helper(word_start, index, my_words)
-  (word_start / 2).times do |count|
-    temp = my_words[index - word_start + 1 + count]
-    my_words[index - word_start + 1 + count] = my_words[index - count]
-    my_words[index - count] = temp
+
+def reverse_helper(string_length, start_index, my_words)
+  ((string_length - start_index) / 2).times do |count|
+    temp = my_words[start_index + count]
+    my_words[start_index + count] = my_words[string_length - count - 1]
+    my_words[string_length - count - 1] = temp
   end
-  return my_words
 end
